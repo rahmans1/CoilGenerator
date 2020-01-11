d018a7d1f98d66632b1630cb6657bf841aaee8a9
Sakib <srahman@sapsimserv.physics.umanitoba.ca>
2020-01-11 18:37:45 -0500

    implementing nose of segment4

--- dcoilgen.py
+++ dcoilgen.py
@@ -12,7 +12,7 @@ z_origin=0
 
 # segment 1
 s1_x=2*cm         # extent along x
-s1_y=1*cm         # extent along y
+s1_y=10*cm         # extent along y
 s1_l_arm=(1100-1000)*cm  # length of arms
 s1_rad=(16-4)/2*cm     # arc rad of nose  
 s1_xpos=(12-x_origin)*cm # center of straight part
@@ -21,7 +21,7 @@ s1_theta=math.atan((16.0-14)*cm/s1_l_arm)     # slant angle of upper arm
 
 # segment 2
 s2_x=1*cm 
-s2_y=1*cm 
+s2_y=10*cm 
 s2_l_arm=(1200-1100)*cm # length of arms
 s2_rad=(s1_rad-s1_x+s1_l_arm/2*math.tan(s1_theta)) # arc rad of nose
 #s2_xpos=0*cm # center of straight part
@@ -30,7 +30,7 @@ s2_theta=math.atan((23.0-17)*cm/s2_l_arm)
 
 # segment 3
 s3_x=2*cm
-s3_y=1*cm
+s3_y=10*cm
 s3_l_arm=(1300-1200)*cm
 s3_rad=(s2_rad-s2_x+s2_l_arm/2*math.tan(s2_theta))
 #s3_xpos=0*cm
@@ -39,8 +39,8 @@ s3_theta=math.atan((26.0-23)*cm/s3_l_arm)
 
 # segment 4
 s4_x=s1_x+s2_x+s3_x    # is this correct?
-s4_y_tb=0.5*cm           # thickness of top and bottom pancake
-s4_y_mid=1*cm          # thickness of middle pancake
+s4_y_tb=5*cm           # thickness of top and bottom pancake
+s4_y_mid=10*cm          # thickness of middle pancake
 s4_y=s4_y_tb+s4_y_mid    # total thickness of segment 4 
 s4_l_arm_p1= (1400-1300)*cm    # length of p1. p1 is bottom arm adjacent to nose
 s4_theta_p1=math.atan((6.5-4)*cm/s4_l_arm_p1)   #angle of rise of p1.
@@ -99,15 +99,16 @@ f.write("\t<union name=\"solid_s3_1\">\n\t\t<first ref=\"solid_s3_frontNose\"/>\
 f.write("\t<union name=\"solid_s3\">\n\t\t<first ref=\"solid_s3_1\"/>\n\t\t<second ref=\"solid_s3_lowerArm\"/>\n\t\t<position name=\"pos_s3\" x=\""+str(-s3_rad+s3_x/2-(s1_x+s2_x)/2)+"\" y=\""+str(-s3_l_arm/2)+"\" z=\"0\"/>\n\t\t<rotation name=\"rot_s3\" x=\"pi/2\" y=\"0\" z=\"0\"/>\n\t</union>\n")
 
 
-f.write("\t<union name=\"solid_s4\">\n\t\t<first ref=\"solid_s4_frontNose\"/>\n\t\t<second ref=\"solid_s4_p1_tb\"/>\n\t\t<position name=\"pos_s4_1\" x=\""+str(-s4_rad+s4_x/2)+"\" y=\""+str(-s4_l_arm_p1/2)+"\" z=\"0\"/>\n\t\t<rotation name=\"rot_s4_1\" x=\"pi/2\" y=\"0\" z=\"0\"/>\n\t</union>\n")
-
+f.write("\t<union name=\"solid_s4_1\">\n\t\t<first ref=\"solid_s4_frontNose\"/>\n\t\t<second ref=\"solid_s4_p1_tb\"/>\n\t\t<position name=\"pos_s4_1\" x=\""+str(-s4_rad+s4_x/2)+"\" y=\""+str(-s4_l_arm_p1/2)+"\" z=\"0\"/>\n\t\t<rotation name=\"rot_s4_1\" x=\"pi/2\" y=\"0\" z=\"0\"/>\n\t</union>\n")
+f.write("\t<union name=\"solid_s4_2\">\n\t\t<first ref=\"solid_s4_1\"/>\n\t\t<second ref=\"solid_s4_p1_mid\"/>\n\t\t<position name=\"pos_s4_2\" x=\""+str(-s4_rad+s4_x/2-s4_l_arm_p1/2*math.tan(s4_theta_p1))+"\" y=\""+str(-s4_l_arm_p1/2)+"\" z=\""+str(-(s4_y_tb+s4_y_mid)/2)+"\"/>\n\t\t<rotation name=\"rot_s4_2\" x=\"pi/2\" y=\"0\" z=\"0\"/>\n\t</union>\n")
+f.write("\t<union name=\"solid_s4\">\n\t\t<first ref=\"solid_s4_2\"/>\n\t\t<second ref=\"solid_s4_1\"/>\n\t\t<position name=\"pos_s4_3\" x=\""+str(0)+"\" y=\""+str(0)+"\" z=\""+str(-s4_y_tb-s4_y_mid)+"\"/>\n\t\t<rotation name=\"rot_s4_3\" x=\"0\" y=\"0\" z=\"0\"/>\n\t</union>\n")
 ### Final unions
 f.write("\t<union name=\"solid_s1_s2\">\n\t\t<first ref=\"solid_s1\"/>\n\t\t<second ref=\"solid_s2\"/>\n\t\t<position name=\"pos_s1_s2\" x=\""+str(s1_l_arm/2*math.tan(s1_theta))+"\" y=\""+str(-s1_l_arm)+"\" z=\"0\"/>\n\t\t<rotation name=\"rot_s2\" x=\"0\" y=\"0\" z=\"0\"/>\n\t</union>\n")
 f.write("\t<union name=\"solid_s1_s2_s3\">\n\t\t<first ref=\"solid_s1_s2\"/>\n\t\t<second ref=\"solid_s3\"/>\n\t\t<position name=\"pos_s1_s2_s3\" x=\""+str(s1_l_arm/2*math.tan(s1_theta)+s2_l_arm/2*math.tan(s2_theta))+"\" y=\""+str(-s1_l_arm-s2_l_arm)+"\" z=\"0\"/>\n\t\t<rotation name=\"rot_s2\" x=\"0\" y=\"0\" z=\"0\"/>\n\t</union>\n")
 f.write("\t<union name=\"solid_s1_s2_s3_s4\">\n\t\t<first ref=\"solid_s1_s2_s3\"/>\n\t\t<second ref=\"solid_s4\"/>\n\t\t<position name=\"pos_s1_s2_s3_s4\" x=\""+str(s1_l_arm/2*math.tan(s1_theta)+s2_l_arm/2*math.tan(s2_theta)+s3_l_arm/2*math.tan(s3_theta)+s4_l_arm_p1/2*math.tan(s4_theta_p1))+"\" y=\""+str(-s1_l_arm-s2_l_arm-s3_l_arm)+"\" z=\""+str((s4_y_mid+s4_y_tb)/2)+"\"/>\n\t\t<rotation name=\"rot_s2\" x=\"0\" y=\"0\" z=\"0\"/>\n\t</union>\n")
 
 
-f.write("\t<box name=\"solid_dcoil\" lunit=\"mm\" x=\""+str(360)+"\" y=\""+str(60)+"\" z=\""+str(2000)+"\"/>\n")
+f.write("\t<box name=\"solid_dcoil\" lunit=\"mm\" x=\""+str(800)+"\" y=\""+str(60)+"\" z=\""+str(6000)+"\"/>\n")
 f.write("\t<tube name=\"solid_DS_toroidMother\" rmin=\""+str(0)+"\"  rmax=\""+str(1000)+"\" z=\""+str(8000)+"\" startphi=\"0\" deltaphi=\"360\" aunit=\"deg\" lunit=\"mm\"/>\n")
 f.write("</solids>\n")
 

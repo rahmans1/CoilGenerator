31964f8bc46eb625e8455b55663c1e0e8c4704e5
Sakib <srahman@sapsimserv.physics.umanitoba.ca>
2020-01-18 18:32:26 -0500

    Improve downstream coil generator definition

--- dcoilgen.py
+++ dcoilgen.py
@@ -6,21 +6,17 @@ import math
 output_file="downstream"
 cm=10.0
 
-
-
 # segment 1
 s1_x=(17.25-14.5)*cm         # extent along x
 s1_y=2*cm                    # extent along y
-s1_l_arm=(1100-1000)*cm  # length of arms
-s1_rad=(17.25-4)/2*cm     # arc rad of nose  
+s1_l_arm=(1100-1000)*cm      # length of arms
+s1_rad=(17.25-4)/2*cm        # arc rad of nose  
 s1_theta=math.atan((20.0-17.25)*cm/s1_l_arm)     # slant angle of upper arm
-
-
 
 # segment 2
 s2_x=(17.25-15.9)*cm 
 s2_y=2*cm 
-s2_l_arm=(1200-1100)*cm # length of arms
+s2_l_arm=(1200-1100)*cm                            # length of arms
 s2_rad=(s1_rad-s1_x+s1_l_arm/2*math.tan(s1_theta)) # arc rad of nose
 s2_theta=math.atan((25.45-20)*cm/s2_l_arm)
 
@@ -32,14 +28,14 @@ s3_rad=(s2_rad-s2_x+s2_l_arm/2*math.tan(s2_theta))
 s3_theta=math.atan((29.1-25.45)*cm/s3_l_arm)
 
 # segment 4
-s4_x=s1_x+s2_x+s3_x    # is this correct?
-s4_y_tb=1*cm           # thickness of top and bottom pancake
-s4_y_mid=2*cm          # thickness of middle pancake
-s4_y=2*s4_y_tb+s4_y_mid    # total thickness of segment 4 
-s4_l_arm_low1= (1404-1300)*cm    # length of low1. low1 is bottom arm adjacent to nose.
-s4_l_arm_low2= (1480-1404)*cm    # length of low2
+s4_x=s1_x+s2_x+s3_x           # is this correct?
+s4_y_tb=1*cm                  # thickness of top and bottom pancake
+s4_y_mid=2*cm                 # thickness of middle pancake
+s4_y=2*s4_y_tb+s4_y_mid       # total thickness of segment 4 
+s4_l_arm_low1= (1404-1300)*cm # length of low1. low1 is bottom arm adjacent to nose.
+s4_l_arm_low2= (1480-1404)*cm # length of low2
 s4_theta_low1=math.atan((6.4-4)*cm/s4_l_arm_low1)   #angle of rise of low1.
-s4_l_arm_low3= (1656-1480)*cm  # length of low3.
+s4_l_arm_low3= (1656-1480)*cm # length of low3.
 s4_theta_low3= math.atan((25.8-14.8)*cm/s4_l_arm_low3) 
 s4_l_arm_low4= (1670-1656)*cm # length of low4
 s4_theta_low4= math.atan((25.8-25.6)*cm/s4_l_arm_low4)
@@ -54,7 +50,7 @@ s4_l_arm_up2= (1444-1340)*cm
 s4_theta_up2= math.atan((33.3-30)*cm/s4_l_arm_up2)
 
 s4_l_arm_up3= (1476-1444)*cm
-s4_h_arm_up3= (40-20.8)*cm
+s4_h_arm_up3= (40.2-20.8)*cm
 s4_l_box_up3= (s4_l_arm_up3-s4_h_arm_up3+s4_x)/2
 s4_rad_up3= s4_h_arm_up3/2
 
@@ -68,12 +64,21 @@ s4_rad_end_up=s4_rad-s4_h_arm_up3+s4_x+ s4_l_arm_up1*math.tan(s4_theta_up1)+ s4_
 s4_rad_end_low=-s4_rad+(s4_l_arm_low2)*math.tan(s4_theta_low1)+(s4_l_arm_low3)*math.tan(s4_theta_low3)-(s4_l_arm_low4)*math.tan(s4_theta_low4)
 s4_rad_end=(s4_rad_end_up-s4_rad_end_low)/2
 
-h_single_coil=(40-4)*cm
+
+h_single_coil=s4_rad_end_up-s4_rad+2*s1_rad+s1_l_arm*math.tan(s1_theta)+s2_l_arm*math.tan(s2_theta)+s3_l_arm*math.tan(s3_theta)
+
 l_single_coil=(s1_rad+s1_l_arm+s2_l_arm+s3_l_arm+s4_l_arm_up1+s4_l_arm_up2+s4_l_arm_up3+s4_l_arm_up4+s4_l_arm_up5+s4_rad_end)
 x_origin=s1_rad-h_single_coil/2
 z_origin= s1_rad-l_single_coil/2
 
-print(str(s1_x)+" "+str(s2_x)+" "+str(s3_x)+" "+ str(s4_x)+" "+str(s4_l_arm_up3)+" "+str(s4_rad_up3))
+
+pos=4*cm+h_single_coil/2
+
+r_inner_mother=pos-h_single_coil/2-10
+r_outer_mother=pos+h_single_coil/2+10
+l_mother=l_single_coil
+
+
 f=open(output_file+".gdml", "w+")
 
 
@@ -150,8 +155,8 @@ f.write("\t<union name=\"solid_s1_s2_s3\">\n\t\t<first ref=\"solid_s1_s2\"/>\n\t
 f.write("\t<union name=\"solid_s1_s2_s3_s4\">\n\t\t<first ref=\"solid_s1_s2_s3\"/>\n\t\t<second ref=\"solid_s4\"/>\n\t\t<position name=\"pos_s1_s2_s3_s4\" x=\""+str(s1_l_arm/2*math.tan(s1_theta)+s2_l_arm/2*math.tan(s2_theta)+s3_l_arm/2*math.tan(s3_theta)+s4_l_arm_low1/2*math.tan(s4_theta_low1))+"\" y=\""+str(-s1_l_arm-s2_l_arm-s3_l_arm)+"\" z=\""+str((s4_y_mid+s4_y_tb)/2)+"\"/>\n\t\t<rotation name=\"rot_s1_s2_s3_s4\" x=\"0\" y=\"0\" z=\"0\"/>\n\t</union>\n")
 
 
-f.write("\t<box name=\"solid_dcoil\" lunit=\"mm\" x=\""+str(h_single_coil+2)+"\" y=\""+str(s4_y+2)+"\" z=\""+str(l_single_coil+2)+"\"/>\n")
-f.write("\t<tube name=\"solid_DS_toroidMother\" rmin=\""+str(0)+"\"  rmax=\""+str(1000)+"\" z=\""+str(8000)+"\" startphi=\"0\" deltaphi=\"360\" aunit=\"deg\" lunit=\"mm\"/>\n")
+f.write("\t<box name=\"solid_dcoil\" lunit=\"mm\" x=\""+str(h_single_coil)+"\" y=\""+str(s4_y)+"\" z=\""+str(l_single_coil)+"\"/>\n")
+f.write("\t<tube name=\"solid_DS_toroidMother\" rmin=\""+str(r_inner_mother)+"\"  rmax=\""+str(r_outer_mother)+"\" z=\""+str(l_mother)+"\" startphi=\"0\" deltaphi=\"360\" aunit=\"deg\" lunit=\"mm\"/>\n")
 f.write("</solids>\n")
 
 
@@ -169,7 +174,7 @@ for i in range(0,7):
 f.write("\t<volume name=\"DS_toroidMother\">\n\t\t<materialref ref=\"G4_Galactic\"/>\n\t\t<solidref ref=\"solid_DS_toroidMother\"/>\n")
 
 for i in range(0,1):
-        rpos=0
+        rpos=pos
         theta=2*i*math.pi/7
         xpos=rpos*(math.cos(theta))
         ypos=rpos*(math.sin(theta)) 
